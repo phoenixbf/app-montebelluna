07fe884154a0d66c3148923f864ea9accf9081d9
Bruno Fanini <phoenixbf@gmail.com>
2021-02-11 19:39:45 +0100

    first update

---
+++ js/main.js
@@ -0,0 +1,13 @@
+/*
+	Main js entry for template ATON web-app
+
+===============================================*/
+window.addEventListener( 'load', ()=>{
+
+    // Realize the base front-end
+    //ATON.FE.realize();
+
+    // Our events
+	//ATON.FE.addBasicLoaderEvents();
+
+});
